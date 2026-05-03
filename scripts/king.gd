18a70739d1f5426be92fc45f42e441ae79ce300e
optus-56 <bijanbakabal45@gmail.com>
2026-05-03 14:38:39 +0545

    more mobs

--- scripts/king.gd
+++ scripts/king.gd
@@ -15,8 +15,8 @@ const HEALTH_BAR_HEIGHT := 4
 const HEALTH_BAR_OFFSET_Y := -62  # Offset above the mob
 
 # HP + reactions
-var max_hp: int = 6
-var hp: int = 3
+var max_hp: int = 10
+var hp: int = 10
 var dead: bool = false
 
 const HURT_LOCK_TIME: float = 0.1
